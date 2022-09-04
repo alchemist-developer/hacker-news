7bcafb91d73199fdb0443db0dc32bdd03d4388af
Adryel Simon <adryel@live.it>
2022-09-04 19:49:34 -0300

    chore: project base

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+
+function header() {
+  return (
+    <>
+      <h1>Hacker News Clone</h1>
+      <div className="nav-link">
+        <NavLink to="/top" activeClassName="active">
+          Top Stories
+        </NavLink>
+        <NavLink to="/new" activeClassName="active">
+          Latest Stories
+        </NavLink>
+        <NavLink to="/best" activeClassName="active">
+          Best Stories
+        </NavLink>
+      </div>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/components/PageNotFound/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <>
+      <p>
+        Page Not Found. Go to <Link to="/">Home</Link>{" "}
+      </p>
+    </>
+  );
+};
+
+export default PageNotFound;
