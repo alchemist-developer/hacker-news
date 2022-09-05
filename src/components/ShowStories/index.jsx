92e8dc3dd830da4f13ad3c4145788fb3bccab8ee
Adryel Simon <adryel@live.it>
2022-09-04 22:15:06 -0300

    feat: add a loading overlay

--- src/components/ShowStories/index.jsx
+++ src/components/ShowStories/index.jsx
@@ -1,20 +1,18 @@
 import Story from "../Story";
 import useDataFetcher from "../../hooks/dataFetcher";
+import Loader from "../Loader";
 
 const ShowStories = ({ type }) => {
-    const { isLoading, stories } = useDataFetcher(type ? type : 'top');
+  const { isLoading, stories } = useDataFetcher(type ? type : "top");
 
   return (
     <>
-      {isLoading ? (
-        <p className="loading">Loading...</p>
-      ) : (
-        <>
-          {stories.map(({ data: story }) => (
-            <Story key={story.id} story={story} />
-          ))}
-        </>
-      )}
+      <Loader show={isLoading}>Loading...</Loader>
+      <>
+        {stories.map(({ data: story }) => (
+          <Story key={story.id} story={story} />
+        ))}
+      </>
     </>
   );
 };
